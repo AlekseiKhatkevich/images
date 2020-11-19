7dfedf23fddb02e3721539930ded30c530321366
Aleksei Khatkevich <alekseikhatkevich@gmail.com>
2020-11-19 16:21:02 +0300

    конец

--- handle_images.py
+++ handle_images.py
@@ -1,3 +1,4 @@
+import argparse
 from pathlib import Path
 from typing import Container
 
@@ -8,59 +9,98 @@ class AddSignature:
     """
     Добавляет подпись в изображения сохраняя их в отдельную папку.
     """
-    font_size = 30
-    title_font = ImageFont.truetype('fonts/PlayfairDisplay-Regular.ttf', font_size)
-    title_text = 'Aleksei Khatkevich'
-    color = (237, 230, 211)
+    def __init__(self,
+                 title_font: str = 'fonts/PlayfairDisplay-Regular.ttf',
+                 title_text: str = 'Aleksei Khatkevich',
+                 color: tuple = (237, 230, 211),
+                 source_folder: str = 'source-images',
+                 output_folder: str = 'output-images',
+                 extensions: Container[str] = ('jpg',),
+                 max_font_size: int = 50,
+                 ) -> None:
+        """
+        :param title_font: размер шрифта
+        :param title_text: текст добавляемый на изображение
+        :param color: цвет текста
+        :param source_folder: папка с исходными изображениями
+        :param output_folder: папка с подписанными изображениями
+        :param extensions: допустимые расширения файлов
+        :param max_font_size: максимальный размер шрифта
+        """
+        self.title_font = title_font
+        self.title_text = title_text
+        self.color = color
+        self.source_folder = Path(source_folder)
+        self.output_folder = Path(output_folder)
+        self.extensions = extensions
+        self.max_font_size = max_font_size
 
-    @staticmethod
-    def get_files(source_folder: Path,
-                  extensions: Container[str],
-                  ) -> set:
+    def get_files(self, source_folder: Path) -> set:
         """
         Отдает сет с путями к файлам.
-        extensions - список расширений файлов. Только файлы с данными расширениями будут найдены.
         """
         files_path = set()
         for file_path in source_folder.iterdir():
-            if file_path.suffix[1:] in extensions:
+            if file_path.suffix[1:] in self.extensions:
                 files_path.add(file_path)
 
         return files_path
 
-    @classmethod
-    def add_text(cls, file_path: Path, output_folder: Path) -> None:
+    def add_text(self, file_path: Path) -> None:
         """
-        Добавляет подпись в изображение и сохраняет его в  'output_folder'.
+        Добавляет подпись в изображение и сохраняет его в 'output_folder'.
         """
         with Image.open(file_path) as image:
+            # Линейные размеры изображения
             image_w, image_h = image.size
             image_editable = ImageDraw.Draw(image)
-            text_w, text_h = image_editable.textsize(cls.title_text, cls.title_font)
-
-            x_pos = image_w - text_w - 10
-            y_pos = image_h - text_h - 10
+            # Размер шрифта зависит от размеров изображения, но не более "max_font_size"
+            font_size = min(
+                self.max_font_size,
+                int((image_w * image_h) / 19000),
+            )
+            title_font = ImageFont.truetype(self.title_font, font_size)
+            # Линейные размеры текста.
+            text_w, text_h = image_editable.textsize(self.title_text, title_font)
+            # Координаты расположения текста
+            x_pos = (image_w - text_w) * 0.98
+            y_pos = (image_h - text_h) * 0.98
 
             image_editable.text(
                 (x_pos, y_pos),
-                cls.title_text,
-                cls.color,
-                font=cls.title_font,
+                self.title_text,
+                self.color,
+                font=title_font,
             )
 
-            image.save(output_folder / file_path.name)
+            image.save(self.output_folder / file_path.name)
 
-    def __call__(self,
-                 source_folder: str = 'source-images',
-                 extensions: Container[str] = ('jpg',),
-                 output_folder: str = 'output-images',
-                 ) -> None:
+    def __call__(self) -> None:
         """
+        Запускает весь процесс:
+        1) Создаем папку "output_folder" если нужно.
+        2) Получаем список файлов.
+        3) Создаем новые файлы с подписями и сохраняем их.
         """
-        source_folder = Path(source_folder)
-        output_folder = Path(output_folder)
 
-        files = self.get_files(source_folder, extensions)
-        for file in files:
-            self.add_text(file, output_folder)
+        Path(self.output_folder).mkdir(parents=True, exist_ok=True)
 
+        files_path = self.get_files(self.source_folder)
+        for path in files_path:
+            self.add_text(path)
+
+
+if __name__ == '__main__':
+    parser = argparse.ArgumentParser(
+        description='Выбор возможного пути сохранения подписанных изображений.',
+    )
+    parser.add_argument(
+        '--path',
+        metavar='--path',
+        type=str,
+        help='Альтернативный путь сохранения подписанных изображений',
+        default=Path('output-images')
+    )
+    args = parser.parse_args()
+
+    AddSignature(output_folder=args.path)()
